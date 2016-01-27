e77cb4c9ca1e542d94529a7e75f744ba38dcf870
Hans <hans@Hanss-MacBook-Pro.local>
2016-01-26 19:57:14 -0700

    todays work....

---
+++ src/assets/js/app.js
@@ -0,0 +1,77 @@
+$(document).foundation();
+
+(function ($) {
+  "use strict";
+  function onScrollAnimations() {
+    $('.wp-1').waypoint(function() {
+      $('.wp-1').addClass('animated fadeInUp');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-2').waypoint(function() {
+      $('.wp-2').addClass('animated fadeInUp');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-3').waypoint(function() {
+      $('.wp-3').addClass('animated fadeInUp');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-4').waypoint(function() {
+      $('.wp-4').addClass('animated fadeIn');
+    }, {
+      offset: '75%'
+    });
+    $('.wp-5').waypoint(function() {
+      $('.wp-5').addClass('animated fadeInRight');
+    }, {
+      offset: '50%'
+    });
+    $('.wp-6').waypoint(function() {
+      $('.wp-6').addClass('animated fadeInLeft');
+    }, {
+      offset: '50%'
+    });
+    $('.wp-7').waypoint(function() {
+      $('.wp-7').addClass('animated fadeInUp');
+    }, {
+      offset: '60%'
+    });
+    $('.wp-8').waypoint(function() {
+      $('.wp-8').addClass('animated fadeInUp');
+    }, {
+      offset: '60%'
+    });
+  }
+
+  function htmlVideo() {
+    videojs("demo_video", {
+      controlBar: {
+        timeDivider: false,
+        fullscreenToggle: false,
+        playToggle: false,
+        remainingTimeDisplay: false
+      },
+      "height": "auto",
+      "width": "auto"
+    }).ready(function() {
+      var myPlayer = this;
+      var aspectRatio = 5 / 12; // aspect ratio 12:5 (video frame 960x400)
+      function resizeVideoJS() {
+          var width = document.getElementById(myPlayer.id()).parentElement.offsetWidth;
+          myPlayer.width(width).height(width * aspectRatio);
+      }
+      resizeVideoJS();
+      window.onresize = resizeVideoJS;
+    });
+  }
+
+  function init() {
+    onScrollAnimations();
+    htmlVideo();
+  }
+
+  init();
+
+})(jQuery);
